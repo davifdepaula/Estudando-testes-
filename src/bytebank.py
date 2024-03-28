f380152379a516365b641edc244c5c826975d112
Davi <davi.ferreira100@gmail.com>
2024-03-28 00:14:29 -0300

    refactor: refactored calcular_bonus function

--- src/bytebank.py
+++ src/bytebank.py
@@ -34,8 +34,8 @@ class Funcionario:
     def calcular_bonus(self):
         valor = self._salario * 0.1
         if valor > 1000:
-            valor = 0
+            raise Exception('Salário muito alto para receber bônus')
         return valor
 
     def __str__(self):
-        return f'Funcionario({self._nome}, {self._data_nascimento}, {self._salario})'
+        return f'Funcionario({self._nome}, {self._data_nascimento}, {self._salario}, {self._cargo})'
