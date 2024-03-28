5018733f8ee587ff36f4edc56073f8a6f77faa8f
Davi <davi.ferreira100@gmail.com>
2024-03-27 23:15:19 -0300

    added tests for sobrenome function

--- tests/test_bytebank.py
+++ tests/test_bytebank.py
@@ -5,7 +5,7 @@ from src.bytebank import Funcionario
 
 class TestClass:
 
-    def test_when_data_13_10_2000_should_return_22(self):
+    def test_when_idade_receive_13_10_2000_should_return_22(self):
         birth_day = '05/09/1993'
         expect = 30
 
@@ -14,3 +14,15 @@ class TestClass:
         age = employer_test.idade()
 
         assert age == expect
+
+    def test_when_sobrenome_receive_Lucas_Carvalho_should_return_Carvalho(self):
+        entry = 'Lucas Carvalho'
+        expect = 'Carvalho'
+
+        employer_test = Funcionario(entry, '13/10/2000', 1111)
+
+        sobrenome = employer_test.sobrenome()
+
+        assert sobrenome == expect
+
+
